63b6464b19893ec3e99b36ac06714e3db66b6995
Luis Cabrera Benito <parzibyte@gmail.com>
2021-04-18 09:31:43 -0500

    Formateando fecha a DD-MM-YYYY

--- models/productos.js
+++ models/productos.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     obtener() {
         return new Promise((resolve, reject) => {
-            conexion.query(`select id, nombre, precio, fecuc from productos`,
+            conexion.query(`select id, nombre, precio, date_format(fecuc, "%d-%m-%Y") as fecuc from productos`,
                 (err, resultados) => {
                     if (err) reject(err);
                     else resolve(resultados);
