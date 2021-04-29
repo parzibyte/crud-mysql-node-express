2752255cd8174ecaa620c41eb1003338ffbba1b5
Luis Cabrera Benito <parzibyte@gmail.com>
2021-04-29 16:33:37 -0500

    Soportando precios más altos

--- esquema.sql
+++ esquema.sql
@@ -2,7 +2,7 @@ create database tienda;
 create table productos(
     id integer not null auto_increment,
     nombre varchar(255),
-    precio decimal(5, 2),
+    precio decimal(9, 2),
     fecuc varchar(10),
     primary key(id)
 );
