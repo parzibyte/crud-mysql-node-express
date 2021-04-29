446df875b153d0378100ce69222b82149feafc72
Luis Cabrera Benito <parzibyte@gmail.com>
2021-04-29 17:07:55 -0500

    Usuario por defecto

--- esquema.sql
+++ esquema.sql
@@ -12,3 +12,9 @@ create table usuarios(
     palabra_secreta varchar(255) NOT NULL,
     primary key(id)
 );
+/*
+Usuario por defecto
+Correo: parzibyte@gmail.com
+Contraseña: 123
+*/
+INSERT INTO `usuarios`(correo, palabra_secreta) VALUES ('parzibyte@gmail.com','$2a$10$7h671u1hAW.8NvbXB6zLw.gD3qxPcSbkR7lUHqIcvTEf1dcbTiePe');
